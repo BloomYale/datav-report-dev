4a62acebae80ac45e98679cbd77e2ded6e376bed
BloomYale <923240066@qq.com>
2025-11-10 10:15:44 +0800

    feat:顶部组件整体开发完成

--- src/components/TodayUser/index.vue
+++ src/components/TodayUser/index.vue
@@ -1,11 +1,43 @@
 <template>
-    <common-card></common-card>
+    <common-card title="今日交易用户数" value="81,014">
+        <template v-slot>
+            <div id="today-users-chart" :style="{ width: '100%', height: '100%' }">
+            </div>
+        </template>
+        <template v-slot:footer>
+            <span class="emphasis">退货率：5.14%</span>
+        </template>
+    </common-card>
 </template>
 
 <script setup>
 import CommonCard from '../CommonCard/index.vue';
+import * as echarts from 'echarts'
+import { ref, onMounted } from 'vue'
+onMounted(() => {
+    const chartDom = document.getElementById('today-users-chart')
+    const chart = echarts.init(chartDom)
+    chart.setOption({
+        color: ['#3398DB'],
+        xAxis: {
+            type: 'category',
+            data: ['00:00', '01:00', '02:00', '03:00', '04:00', '06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00', '22:00'],
+            show: false
+        },
+        yAxis: {
+            show: false
+        },
+        series: [{
+            type: 'bar',
+            data: [410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 134, 290, 330, 150],
+            barWidth: '60%',
+        }],
+        grid: {
+            top: 0, bottom: 0, left: 0, right: 0
+        }
+    })
+
+})
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -1,11 +1,108 @@
 <template>
-    <common-card></common-card>
+    <common-card title="累计用户数" value="1,087,503">
+        <template v-slot>
+            <div id="total-users-chart" :style="{ width: '100%', height: '100%' }">
+            </div>
+        </template>
+        <template v-slot:footer>
+            <div class="total-users-footer">
+                <span>日同比</span>
+                <span class="emphasis">8.73%</span>
+                <div class="increase"></div>
+                <span class="month">月同比</span>
+                <span class="emphasis">35.91%</span>
+                <div class="decrease"></div>
+            </div>
+        </template>
+    </common-card>
 </template>
 
 <script setup>
 import CommonCard from '../CommonCard/index.vue';
+import * as echarts from 'echarts'
+import { ref, onMounted } from 'vue'
+onMounted(() => {
+    const chartDom = document.getElementById('total-users-chart')
+    const chart = echarts.init(chartDom)
+    chart.setOption({
+        color: ['#45c946', '#eaeaea'],
+        xAxis: {
+            type: 'value',
+            show: false,
+        },
+        yAxis: {
+            type: 'category',
+            show: false,
+        },
+        series: [{
+            type: 'bar',
+            data: [350],
+            stack: 'total',
+            barWidth: '10',
+        }, {
+            type: 'bar',
+            data: [200],
+            stack: 'total'
+        }, {
+            type: 'custom',
+            data: [350],
+            renderItem: function (params, api) {
+                var xValue = api.value(0);
+                const endPoint = api.coord([xValue, 0]);
+                return {
+                    type: 'group',
+                    position: endPoint,
+                    children: [
+                        {
+                            type: 'path',
+                            shape: {
+                                d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z', //svg path data
+                                x: -10,
+                                y: 2,
+                                width: 20,
+                                height: 20,
+                                layout: 'center'
+                            },
+                            style: {
+                                fill: '#45c946'
+                            },
+                        },
+                        {
+                            type: 'path',
+                            shape: {
+                                d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z', //svg path data
+                                x: -10,
+                                y: -22,
+                                width: 20,
+                                height: 20,
+                                layout: 'center'
+                            },
+                            style: {
+                                fill: '#45c946'
+                            },
+
+                        }
+                    ],
+                };
+            },
+        }],
+        grid: {
+            top: 0, bottom: 0, left: 0, right: 0
+        }
+    })
+
+})
 </script>
 
 <style lang="scss" scoped>
+.total-users-footer {
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    color: #666;
 
+    .month {
+        margin-left: 10px;
+    }
+}
 </style>
