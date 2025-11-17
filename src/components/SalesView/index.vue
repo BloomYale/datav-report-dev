55c8ddafdedf3c8aa9a12c7b744d0e3af9c70931
BloomYale <923240066@qq.com>
2025-11-17 19:01:19 +0800

    feat: 销售趋势图标及列表的开发

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="sales-view">
-        <el-card shadow="hover">
+        <el-card shadow="hover" :body-style="{ padding: '0 0 20px 0' }">
             <template #header>
                 <div class="menu-wrapper">
                     <el-menu mode="horizontal" :default-active="activeIndex" @select="onMenuSelect"
@@ -20,11 +20,28 @@
                     </div>
                 </div>
             </template>
+            <template #default>
+                <div class="sales-view-chart-wrapper">
+                    <v-chart :option="chartOption"></v-chart>
+                    <div class="sales-view-list">
+                        <div class="sales-view-title">排行榜</div>
+                        <div class="list-item-wrapper">
+                            <div class="list-item" v-for="item in rankData" :key="item.no">
+                                <span :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">{{ item.no }}</span>
+                                <span class="list-item-name">{{ item.name }}</span>
+                                <span class="list-item-money">{{ item.money }}</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </template>
         </el-card>
     </div>
 </template>
 
 <script setup>
+import { color } from 'echarts';
 import { ref } from 'vue';
 const activeIndex = ref('1');
 const onMenuSelect = (index) => {
@@ -61,6 +78,65 @@ const shortcuts = [
         },
     },
 ]
+const rankData = ref([{ no: 1, name: '麦当劳', money: 323234 }, { no: 2, name: '麦当劳', money: 323234 }, { no: 3, name: '麦当劳', money: 323234 }, { no: 4, name: '麦当劳', money: 323234 }, { no: 5, name: '麦当劳', money: 323234 },]);
+const chartOption = ref({
+    title: {
+        text: '年度销售额',
+        textStyle: {
+            fontSize: 12,
+            color: '#666'
+        },
+        left: 20,
+        top: 20
+    },
+    xAxis: {
+        type: 'category',
+        axisTick: {
+            show: true,
+            alignWithLabel: true,
+            lineStyle: {
+                color: '#999'
+            }
+        },
+        axisLine: {
+            lineStyle: {
+                color: '#999'
+            }
+        },
+        axisLabel: {
+            color: '#333'
+        },
+        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+    },
+    yAxis: {
+        axisLine: {
+            show: false
+        },
+        axisTick: {
+            show: false
+        },
+        splitLine: {
+            lineStyle: {
+                type: 'dotted',
+                color: '#eee'
+            }
+        }
+    },
+    grid: {
+        left: 70,
+        right: 60,
+        left: 60,
+        bottom: 50
+    },
+    series: [
+        {
+            data: [120, 200, 150, 80, 70, 110, 130, 90, 160, 200, 180, 220],
+            type: 'bar',
+            barWidth: '35%',
+        }
+    ],
+    color: ['#3398DB'],
+});
 </script>
 
 <style lang="scss" scoped>
@@ -90,6 +166,73 @@ const shortcuts = [
             align-items: center;
             justify-content: flex-end;
             gap: 20px;
+        }
+    }
+
+    .sales-view-chart-wrapper {
+        display: flex;
+        height: 270px;
+
+        .echarts {
+            flex: 0 0 70%;
+            width: 70%;
+            height: 100%;
+        }
+
+        .sales-view-list {
+            flex: 1;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            overflow: hidden;
+
+
+            .sales-view-title {
+                font-size: 12px;
+                margin-top: 28px;
+                color: #666;
+                font-weight: 500;
+            }
+
+            .list-item-wrapper {
+                margin-top: 15px;
+
+                .list-item {
+                    display: flex;
+                    align-items: center;
+                    padding: 6px 20px 6px 0;
+                    border-bottom: 1px solid #f0f0f0;
+                    font-size: 12px;
+
+                    .list-item-no {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        width: 20px;
+                        height: 20px;
+                        color: #333;
+
+                        &.top-no {
+                            background-color: #000;
+                            border-radius: 50%;
+                            color: #fff;
+                            font-weight: 500;
+                        }
+                    }
+
+                    .list-item-name {
+                        margin-left: 10px;
+                        color: #333;
+                    }
+
+                    .list-item-money {
+                        flex: 1;
+                        text-align: right;
+                    }
+                }
+            }
+
         }
     }
 }
